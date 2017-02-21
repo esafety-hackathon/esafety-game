6db909f5f9b6593f5b6a4e202cc1fb0f73e4e3ff
Vishal Mistry <vishalmistry51@gmail.com>
2017-02-21 13:08:02 +0000

    Modified Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,7 +2,7 @@
     <div id="indexview">
       <h1>The e-Safety Game</h1>
       <p>Learn how to be safe online</p>
-      <button @click="next">Play</button>
+      <button class="circle" @click="next">Play</button>
     </div>
 </template>
 
@@ -23,5 +23,19 @@ export default {
 }
 h1 {
   margin: 0;
+}
+.circle {
+  width: 72px;
+  height: 72px;
+  line-height: 72px;
+  background: white;
+  border-radius: 36px;
+  text-align: center;
+  background-size: 72px 72px;
+  display: inline-block;
+  font-size: 24px;
+  font-weight: bold;
+  color: rgba(0,0,0,0);
+  margin-right: 8px;
 }
 </style>
