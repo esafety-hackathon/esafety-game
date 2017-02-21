2c42fc29fcc238215773455cea444b1ef5ce6d86
Amish Shah <amishshah.2k@gmail.com>
2017-02-21 14:50:46 +0000

    things

--- src/views/SectionViewer.vue
+++ src/views/SectionViewer.vue
@@ -2,25 +2,35 @@
 <div class="sectionViewer">
 <h1>{{ section.name }}</h1>
 <p>{{ section.desc }}</p>
-<router-view @next="next"></router-view>
+<button @click="next">play</button>
+<router-view @next="next" :key="$route.params.question"></router-view>
 </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      currentQ: -1,
       section: this.$root.data.sections[this.$route.params.name]
+    }
+  },
+  computed: {
+    question() {
+      return Number(this.$route.params.question);
     }
   },
   methods: {
     next() {
-      const current = this.section.activities[this.currentQ];
-      const next = this.section.activities[this.currentQ+1];
+      let q = this.question;
+      if (isNaN(q)) q = -1;
+      const current = this.section.activities[q];
+      const next = this.section.activities[q ? q + 1 : 0];
       if (!next) return this.done();
       if (next.type === 'truefalse') {
-
+        this.$router.push(`/section/${this.$route.params.name}/truefalse/${q + 1}`);
       }
+    },
+    done() {
+      this.$router.push(`/dashboard`);
     }
   }
 }
