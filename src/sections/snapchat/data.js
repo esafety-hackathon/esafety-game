898a479c922656f1d525e70f28508cd6a16ceab3
Amish Shah <amishshah.2k@gmail.com>
2017-02-22 13:12:05 +0000

    snapchat

--- src/sections/snapchat/data.js
+++ src/sections/snapchat/data.js
@@ -1,3 +1,9 @@
 module.exports =
 [
+  [
+    'How do I change who can view my story on Snapchat?',
+    `Go to settings and look at _View my story_. You can choose anyone, your friends or a custom set of people.
+    
+<img src="/static/articles/snapchat/storyoptions.png" width="300">`
+  ]
 ];
