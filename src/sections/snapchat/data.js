b635bbd8ccbca1bf1fae65da4f79befc5c9da5a4
Vishal Mistry <vishalmistry51@gmail.com>
2017-02-22 13:25:32 +0000

    snapchat pass change

--- src/sections/snapchat/data.js
+++ src/sections/snapchat/data.js
@@ -1,6 +1,11 @@
 module.exports =
 [
   [
+    `How do I change my password on snapchat?`,
+    `Settings ➡️️ Password ➡️️ Enter Current password ➡️️ Enter new password`
+    `password Snapchat privacys`
+  ],
+  [
     'How do I change who can view my story on Snapchat?',
     `Go to settings and look at _View my story_. You can choose anyone, your friends or a custom set of people.
     
