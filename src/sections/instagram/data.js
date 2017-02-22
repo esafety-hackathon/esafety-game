0c1f8136d6d5dd9f4836c6d6594465a1863c3343
Amish Shah <amishshah.2k@gmail.com>
2017-02-22 13:06:27 +0000

    stuff

--- src/sections/instagram/data.js
+++ src/sections/instagram/data.js
@@ -1,25 +1,18 @@
 module.exports =
 [
   [
-    `What does my profile look like to the public?`,
-    `You can simply click [here](https://www.facebook.com/profile.php?viewas=100000686899395) to see what your profile would look like
-to the public.`,
-    'privacy public safe'],
-  [
-    `What is appropriate for me to post?`,
-`It depends, you might want some people to see certain posts, and others not to - you can configure your settings when posting messages
-to handle this. Make sure you aren't just posting stuff blindly for the public to see unless you're sure!
-
-![Post specifically](/static/articles/facebook/post-specific.png)`,
-    'privacy public appropriate apt settings safe'],
-  [
     `How do I change my password on Instagram?`,
 `To change your password on Instagram first go to the top right Options Menu, then go to Change Password, 
 then enter your old password and enter yor new password twice. Tada, you have now changed your password.
 
-<img src="/static/articles/instagram/password-change.png" width="200px"/> `]
-],
+<img src="/static/articles/instagram/password-change.png" width="200px"/> `],
+  [
+    `How do I change who can follow me on Instagram?`,
+`Go to the options menu and toggle private account
+
+<img src="/static/articles/instagram/private-account.png" width="200px"/> `],
   [
     `Who should I follow on instagram?`,
 `On Instagram you should only age appropriate pages and people who are posting things toat are appropriate for you. You should
- not look at anti-Semetic images for example.`];
+ not look at anti-Semetic images for example.`]
+];
